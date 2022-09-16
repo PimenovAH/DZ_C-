3430273b92b8db3618db888d89b839d0dfb11033
Alexandr <pimenov_1998@list.ru>
2022-09-16 21:20:22 +0300

    Задача номер 1

---
+++ Task_One/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Нахождение min и max числа");
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if  (a > b)
+    Console.WriteLine("max = "+ a + "; min = " + b );
+    
+else if ( a< b)
+    Console.WriteLine("max = "+ b + "; min = " + a );
+
+else
+    Console.WriteLine("Числа равны");
