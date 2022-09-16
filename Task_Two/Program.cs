c2a5c53f20586c737bbe9eeef96e95d8dbbc8811
Alexandr <pimenov_1998@list.ru>
2022-09-16 21:26:05 +0300

    Задача номер 2

---
+++ Task_Two/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Проверка на чётность ");
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine(" Чётное: " + a); 
+}
+else
+{
+    Console.WriteLine(" Не чётное: " + a);
+}
