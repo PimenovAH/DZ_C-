5d0339973dbe5d3b3ec81d53cfc69f7ad9314ad1
Alexandr <pimenov_1998@list.ru>
2022-09-16 21:31:27 +0300

    Задача номер 3

---
+++ Task_Three/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Поиск наибольшего из трех чисел");
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите вторе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    Console.Write("max = " + a);
+}
+else if (b > c || c < b)
+{
+    Console.Write("max = " + b);
+}
+else
+{
+    Console.Write("max = " + c);
+}
