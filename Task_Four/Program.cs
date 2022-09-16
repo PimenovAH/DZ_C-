216869195e2ba45dcdd97bda65f74ea5ae26fe5f
Alexandr <pimenov_1998@list.ru>
2022-09-16 21:51:07 +0300

    Задача номер 4

---
+++ Task_Four/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Нахождение кол-во четных от 1 до N");
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N ; i++)
+{
+    if(i % 2 == 0)
+    Console.Write(i + ",");
+                
+}
