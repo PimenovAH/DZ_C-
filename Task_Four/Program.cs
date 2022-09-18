956fd522086857b27cec1ac1025848d10f03b590
Alexandr <pimenov_1998@list.ru>
2022-09-18 18:38:03 +0300

    Избавился от запятой, двумя способами

--- Task_Four/Program.cs
+++ Task_Four/Program.cs
@@ -5,9 +5,20 @@ Console.WriteLine("Нахождение кол-во четных от 1 до N")
 Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
+//List <int> zap = new List<int>();
+
 for (int i = 1; i <= N ; i++)
 {
+      
     if(i % 2 == 0)
-    Console.Write(i + ",");
-                
+    {    
+    Console.Write($"{i},");
+     //zap.Add(i);  
+    } 
+    if( i == N )
+    {
+    Console.Write(N);
+    }
+    
 }
+//Console.Write(string.Join(", ", zap));
