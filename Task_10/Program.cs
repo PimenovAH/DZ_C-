76959cd8bd847dabd6c808fbda65a4dc6a7aac99
Alexandr <pimenov_1998@list.ru>
2022-09-19 23:04:09 +0300

    Вывод втого числа, из трех значных чисел

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿int RandommNumbers3()
+{
+    int number = new Random().Next(100,999);  
+     return number;
+}
+
+
+int Vtoroe(int numbers)
+{
+
+    int result = numbers / 10 %  10;
+    return result;
+}
+
+int a = RandommNumbers3();
+int b =  Vtoroe(a);
+Console.WriteLine(a+ " -> " + b);
