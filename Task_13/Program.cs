58e0bf6bf25e2d3f730d25f7965dd53f97ff0e7e
Alexandr <pimenov_1998@list.ru>
2022-09-20 19:24:40 +0300

    Вывод 3 числа и ошибку об его отсусвие

---
+++ Task_13/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int RandommNumbers3()
+{
+    int numbers = new Random().Next(1,999999);  
+    return numbers;
+}
+
+int a = RandommNumbers3();
+
+
+int NomerRand = a;
+string IntvString = Convert.ToString(a);
+
+if (IntvString.Length > 2)
+{
+  Console.WriteLine(a + " -> " + IntvString[2]);
+}
+else 
+{
+  Console.WriteLine(a + " -> третьей цифры нет: Error!");
+}
