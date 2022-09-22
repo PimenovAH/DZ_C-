b99e0fdbf0d687c153bd9adbb97ea6ef99c9c0ba
Alexandr <pimenov_1998@list.ru>
2022-09-23 00:43:11 +0300

    Проверка на  палиндромом

---
+++ Task_17/Program.cs
@@ -0,0 +1,45 @@
+﻿int num = 0;
+ while(true)
+{
+    try
+    {  
+         
+        Console.WriteLine("Проверка на  палиндромом.");
+        Console.WriteLine();
+        Console.Write("Введите пятизначое число: ");
+        num = int.Parse(Console.ReadLine() ?? "0");
+        break;
+    }
+    catch
+    {
+        
+        Console.WriteLine("Введено некоректное значение");
+    }
+}
+    
+
+int Palynodrome(int num)
+{
+
+int a = 0;
+for(int i = 0; num > 0; i++)
+{
+  a = a * 10 + num % 10; 
+  num /= 10; 
+}
+
+return a;
+}
+
+bool res;
+if(res = (Palynodrome(num) == num))
+{
+    Console.WriteLine($"{num} -> Да ");
+}
+ else
+{
+    Console.WriteLine($"{num} -> нет ");
+}
+
+
+
