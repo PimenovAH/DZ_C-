3cf8ec59bd904ab4ed9de8026800135c6a096e54
Alexandr <pimenov_1998@list.ru>
2022-09-23 19:01:04 +0300

    Находим расстояние между двух точек в 3D пространстве

---
+++ Task_21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double GetNum(string name)
+{
+    Console.WriteLine("Введите координаты " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double x1 = GetNum("X1");
+double x2 = GetNum("X2");
+double y1 = GetNum("Y1");
+double y2 = GetNum("Y2");
+double c1 = GetNum("С1");
+double c2 = GetNum("С2");
+
+double Rastoyn(double x1, double x2, double y1, double y2, double c1, double c2 )
+{
+    double R = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(c2 - c1, 2));
+    return R;
+
+}
+double  tochka =  Rastoyn(x1,x2,y1,y2,c1,c2);
+Console.WriteLine("A"+(x1,y1,c1)+";" + " B"+ (x2,y2,c2) + " -> "+ Math.Round(tochka,2));
