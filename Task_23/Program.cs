06533a2870d41dd866c7fe610a7a41b498d5eab4
Alexandr <pimenov_1998@list.ru>
2022-09-23 19:17:59 +0300

    Таблица кубов от 1 до N

---
+++ Task_23/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int num = 0;
+ while(true)
+{
+    try
+    { 
+        Console.Write("Введите число: ");
+        num = int.Parse(Console.ReadLine() ?? "0");
+        break;
+    }
+    catch
+    {
+        
+        Console.WriteLine("Введено некоректное значение");
+    }
+}
+
+void Kvadrat(int a)
+{
+    int count = 1;
+    while(count <= a)
+    {
+         Console.WriteLine(Math.Pow((count),3) + " \t " + count);
+         count ++;
+    }
+    Console.WriteLine("Таблица кубов чисел  ");
+
+}
+
+    Kvadrat(num);
