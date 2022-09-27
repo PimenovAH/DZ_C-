fc576aafec56ee36af761ed8a4a692d46acf1b54
Alexandr <pimenov_1998@list.ru>
2022-09-27 22:42:24 +0300

    Вывод суммы чисел от числа

---
+++ Task_27/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Raschet()
+{
+
+    int Number()
+    {
+        Console.Write("Введите число: ");
+        return Convert.ToInt32(Console.ReadLine() ?? "0");
+
+    }
+
+
+    int Natur(int A)
+    {
+
+    int result = 0;
+    int b = 0;
+
+        for(int i = 1; i <= A; i++)
+        {    
+         result += (A - b) % 10;  
+         A /= 10;       
+        }
+    
+     return result;  
+    }
+
+    int A = Number();
+
+    int nat = Natur(A);
+
+    Console.Write( $"{A} -> " + nat);
+        
+
+}   
+
+Raschet();
+
+
+
+
