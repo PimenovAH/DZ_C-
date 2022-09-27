8ac0dada19dcc294d24c4379746c5915597e0bc6
Alexandr <pimenov_1998@list.ru>
2022-09-27 19:44:53 +0300

    A в натуральную степень B

---
+++ Task_25/Program.cs
@@ -0,0 +1,30 @@
+﻿int Number()
+{
+        Console.Write("Введите число: ");
+        return Convert.ToInt32(Console.ReadLine() ?? "0");
+
+}
+
+int Natur(int A, int B)
+{
+    int result = 1;
+    for(int i = 1; i <= B; i++)
+    {
+        result *= A;
+        
+    }
+    return result;
+}
+
+int A = Number();
+int B = Number();
+
+int nat = Natur(A,B);
+
+Console.Write( $"{A},{B} -> " + nat);
+
+
+
+
+
+    
