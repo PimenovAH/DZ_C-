d38b8fa924fd2833e38f1bea50fe3f6d1dcb747a
Alexandr <pimenov_1998@list.ru>
2022-09-28 19:33:08 +0300

    Вывод массива

---
+++ Task_29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void R()
+{
+    int Num()
+    {
+        Console.Write("Укажите длину массива: ");
+         
+        return Convert.ToInt32(Console.ReadLine() ?? "0");
+    }
+
+    int b = Num();
+
+    int[] array = new int[b];
+    List<int> siz = new List<int>(b);
+    
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100); 
+        siz.Add(array[i]);             
+    } 
+    
+    Console.Write(string.Join(",", siz));
+    Console.Write(" -> [" );
+    Console.Write(string.Join(",", siz));
+    Console.Write("]");
+}
+R();
