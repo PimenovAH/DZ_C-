c947c3c54015b62fff16135c321a41dcae751ff9
Alexandr <pimenov_1998@list.ru>
2022-09-30 20:27:56 +0300

    Разница между max и min значением

---
+++ Task_38/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.\
+
+void RandomNumbers(int[] array)
+{
+    for(int i = 0; i  < array.Length;i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    List<int> siz = new List<int>(array); 
+    Console.Write("[" );
+    Console.Write(string.Join(",", siz));
+    Console.Write("] -> ");
+}
+
+int MaxNeumberMin(int[] array)
+{
+    int result = 0;
+    int min = array[0];
+    int max =  array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+	    if (array[i] > max)
+	    {
+		    max = array[i];	    
+	    }
+
+        if (array[i] < min)
+	    {
+		    min = array[i];	    
+	    }
+        result = max - min;
+    }
+
+    return  result;
+}
+
+
+int[] mass = new int [5];
+RandomNumbers(mass);
+PrintArray(mass);
+Console.Write("Разница между max и min значением: " + MaxNeumberMin(mass));
