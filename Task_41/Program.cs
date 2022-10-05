5eb6df11f02b9fb5029e5e59dbcffb1c7b1068bd
Alexandr <pimenov_1998@list.ru>
2022-10-06 01:02:35 +0300

    Подсчет положительных чисел

---
+++ Task_41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+double[] FillA()
+{
+    int input = Convert.ToInt32(Console.ReadLine());
+    double[] arr = new double[input];
+    for(int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"Введите число: {i+1}: ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+
+        }
+    return arr;
+}
+
+double ChetArray(double[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length;i++)
+    {
+        if(arr[i] > 0)
+        {
+            count++;
+        }   
+    }
+
+    return count;
+}
+
+
+void PrintArray(double[] array)
+{
+    List<double> siz = new List<double>(array); 
+    Console.Write("[" );
+    Console.Write(string.Join(",", siz));
+    Console.Write("] -> ");
+}
+
+Console.Write("Введите размер массива: ");
+double[] arr = FillA();
+
+Console.WriteLine();
+
+PrintArray(arr);
+Console.Write($"Чисел больше 0 = {ChetArray(arr)}");
+
