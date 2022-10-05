60308e50a99e0ad8d0da54f2bcfb5c14f023982e
Alexandr <pimenov_1998@list.ru>
2022-10-06 02:23:14 +0300

    Первые два числа

---
+++ Task_44/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 44: Не используя рекурсию, выведите
+// первые N чисел Фибоначчи. Первые два числа
+// Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите число: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int a = 0,b= 1;
+double[] numm = new double[size];
+numm [0] = a;
+numm [1] = b;
+for(int i = 2; i < numm.Length;i++)
+    {
+        numm[i] = numm[i-1] + numm[i-2];
+    }
+Console.Write($"{size} -> ");
+PrintArray(numm);
+
+
+
+void PrintArray(double[] array)
+{
+    List<double> siz = new List<double>(array); 
+    Console.Write(" " );
+    Console.Write(string.Join(" ", siz));
+    Console.Write(" ");
+}
+
