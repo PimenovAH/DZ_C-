26e589bec8ff87769f4be2c92efe84a66a3b1a62
Alexandr <pimenov_1998@list.ru>
2022-10-06 13:42:56 +0300

    Точка пересечения

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -14,7 +14,7 @@ Console.Write("Введите k2: " );
 double k2 = Convert.ToInt32(Console.ReadLine());
 
 
-double x = (-b2+b1)/(-k1+k2);
+double x = (b2-b1)/(k1-k2);
 double y = k1 * x + b1;
 
 Console.WriteLine($"Точки пересечения -> ({x}; {y})");
