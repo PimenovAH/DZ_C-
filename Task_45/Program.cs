536ea3ca206a3a97080dee3f5cd889a403f4550e
Alexandr <pimenov_1998@list.ru>
2022-10-06 13:38:15 +0300

    Копия массива

---
+++ Task_45/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Clear();
+
+
+int Number()
+{
+    Console.Write("Введите массив: ");
+    return  Convert.ToInt32(Console.ReadLine());    
+}
+
+
+void RandomArray(int[] array)
+{
+    for(int i = 0; i < array.Length;i++)
+    {
+        array[i] = new Random().Next(-100,100);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    List<int> siz = new List<int>(array); 
+    Console.Write("[" );
+    Console.Write(string.Join(",", siz));
+    Console.Write("]");
+}
+
+int a = Number();
+int[] size = new int[a];
+int[] scan = new int[a];
+
+RandomArray(size);
+
+PrintArray(size);
+
+for(int i = 0; i < size.Length; i ++ )
+{
+    scan[i] = size[i]; 
+
+}
+Console.Write(" <--> ");
+    PrintArray(scan);
+
+Console.WriteLine();
+
+
+
+
+
+
+
+
