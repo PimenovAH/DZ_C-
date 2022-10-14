c64984617221bf33c295fd24c156fa6dbeaea27a
Alexandr <pimenov_1998@list.ru>
2022-10-14 21:11:12 +0300

    Промежуток от N до 1 рекурсия

---
+++ Task_64/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+
+int GetNumberConsole()
+{
+    System.Console.Write("Задайте значение от N до 1: ");
+   
+return Convert.ToInt32(Console.ReadLine());
+}
+
+
+string WriteNumbersRecurs(int start, double num)
+{
+    
+    if(start < num)       
+        return start + ", " + WriteNumbersRecurs(start+1, num);
+
+    return start.ToString();
+}
+
+
+int number = GetNumberConsole();
+System.Console.Write($"N= {number} ---> ");
+System.Console.Write(WriteNumbersRecurs(number, -1));
+
+ 
+
+
